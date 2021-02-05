463224166f0e53bc965eb23a2b941b1a5ce3b6e7
mpb27 <mark.butowski@gmail.com>
2021-02-05 00:19:59 +0000

    feat: Added printing.

--- print.sh
+++ print.sh
@@ -2,4 +2,4 @@
 
 # Prints to PDF file.
 
-reveal-md presentation.md --print presentation.pdf --puppeteer-launch-args="--no-sandbox --disable-dev-shm-usage"
+reveal-md presentation.md --print presentation.pdf --puppeteer-launch-args="--no-sandbox --disable-dev-shm-usage" --port 1949
